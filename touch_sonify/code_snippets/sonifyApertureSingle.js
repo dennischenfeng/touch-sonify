b59df23654063b663e28393d2871ba8dc2110eb2
Dennis Feng <dennisfeng2@gmail.com>
2023-05-25 19:27:44 -0700

    functionalized sonifyAperture with maxNumPoints param

--- touch_sonify/code_snippets/sonifyApertureSingle.js
+++ touch_sonify/code_snippets/sonifyApertureSingle.js
@@ -1,11 +1,14 @@
 // Sonify the data points within the aperture once
 // Assumes xRange, yRange, and dataSource are in the namespace (should be passed in `args` in CustomJS)
 
+let maxNumPoints = 2;
+let terminateBeepAtEnd = false;
 sonifyAperture(
     cb_obj,
     xRange,
     yRange,
     dataSource,
     appVars,
-    maxNumPoints=1,
+    maxNumPoints,
+    terminateBeepAtEnd,
 );
